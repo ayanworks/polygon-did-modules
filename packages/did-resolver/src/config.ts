7eceb1180f74e8b9278600fae258447d0d33e913
Krishna Waske <krishna.waske@ayanworks.com>
2026-03-05 19:19:42 +0530

    fix: config polygon rpc url
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- packages/did-resolver/src/config.ts
+++ packages/did-resolver/src/config.ts
@@ -4,7 +4,7 @@ export const networkConfig = {
     CONTRACT_ADDRESS: '0xcB80F37eDD2bE3570c6C9D5B0888614E04E1e49E',
   },
   mainnet: {
-    URL: 'https://polygon-rpc.com',
+    URL: 'https://polygon.drpc.org',
     CONTRACT_ADDRESS: '0x0C16958c4246271622201101C83B9F0Fc7180d15',
   },
 }
